6ed33080e1048019ddeb03a52fb1483b9838d099
ducloc <locdivine@gmail.com>
2023-11-28 09:19:08 +0700

    aaaaaa

--- app/src/main/java/com/lucky/dsthisinh/DetailStudentActivity.java
+++ app/src/main/java/com/lucky/dsthisinh/DetailStudentActivity.java
@@ -3,6 +3,7 @@ package com.lucky.dsthisinh;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -14,6 +15,9 @@ public class DetailStudentActivity extends AppCompatActivity {
     EditText result;
     LinearLayout layout_student;
 
+    Button btn_update;
+    Button btn_back;
+
     StudentModel student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +27,15 @@ public class DetailStudentActivity extends AppCompatActivity {
         id = findViewById(R.id.id_tv);
         name = findViewById(R.id.name_tv);
         result = findViewById(R.id.result_tv);
+        btn_back = findViewById(R.id.btn_back);
 //        layout_student = findViewById(R.id.layout_student);
 
         id.setText(getIntent().getStringExtra("id"));
         name.setText(getIntent().getStringExtra("name"));
         result.setText(getIntent().getStringExtra("result"));
+
+        btn_back.setOnClickListener(view -> {
+            onBackPressed();
+        });
     }
 }
